fb003fce9ab06c2ce39aba813dfb342639654d5c
Abdul Samad Khan <abdulsamadkhan9897@gmail.com>
2026-09-02 01:36:41 +0530

    Fix path-to-regexp error with /* route

--- backend/server.js
+++ backend/server.js
@@ -111,7 +111,7 @@ if (process.env.NODE_ENV === 'production') {
   
   app.use(express.static(frontendPath));
   
-  app.get('*', (req, res, next) => {
+  app.get('/*', (req, res, next) => {
     if (req.path.startsWith('/api')) {
       return next();
     }
